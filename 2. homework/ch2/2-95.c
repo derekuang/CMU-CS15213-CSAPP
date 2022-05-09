3855dee6a9d47e8d72f15c3dfeda7e56025db113
derekuang <kuangyaode@gmail.com>
2022-05-10 01:20:51 +0800

    homework(信息的表示和处理):2.95提交

--- 2. homework/ch2/2-95.c
+++ 2. homework/ch2/2-95.c
@@ -5,20 +5,88 @@ typedef unsigned float_bits;
 
 float_bits float_half(float_bits f)
 {
-    size_t s = (f >> 31) & 1;
-    size_t e = (f >> 23) & 0xFF;
-    size_t m = f & 0x7FFFFF;
+    size_t s = f & 0x80000000;
+    size_t e = f & 0x7F800000;
+    size_t m = f & 0x007FFFFF;
 
-    printf("%.2X %.2X %.2X", s, e, m);
+    // 规格化的值
+    if (e != 0 && e != 0x7F800000) {
+        if (e == 0x00800000) {
+            size_t em = e | m;
+            em = ((em & 2) == 2) ? em+1 >> 1 : em >> 1;
+            return s | em;
+        }
+        e -= 0x00800000;
+        return s | e | m;
+    }
+    // NaN
+    else if(e == 0x7F800000 && m != 0) {
+        return f;
+    }
+    // 非规格化的值
+    else {
+        m = ((m & 2) == 2) ? m+1 >> 1 : m >> 1;
+        return s | e | m;
+    }
+}
 
-    return f;
+typedef unsigned char* pointer;
+
+void copy_bytes(pointer from, pointer to, size_t len)
+{
+    size_t i;
+    for (i = len-1; i < len; i--)
+        to[i] = from[i];
+}
+
+void show_bytes(pointer start, size_t len)
+{
+    size_t i;
+
+    printf("0x");
+    for (i = len-1; i < len; i--)
+        printf("%.2X", start[i]);
+    printf("\n");
+}
+
+void test_float_half(size_t from, size_t to)
+{
+    unsigned long long i;
+    float x, y;
+    float_bits f;
+
+    printf("testing...\n");
+    for (i = from; i <= to; i++) {
+        f = i;
+
+        copy_bytes((pointer)&f, (pointer)&x, sizeof(float_bits));
+        x *= 0.5;
+
+        f = float_half(f);
+        copy_bytes((pointer)&f, (pointer)&y, sizeof(float_bits));
+
+        if((isnan(x) && isnan(y)) || x == y)
+            ;
+        else {
+            printf("ERROR!\ni = 0x%08X \nx = ", i), show_bytes((pointer)&x, sizeof(x));
+            printf("y = "), show_bytes((pointer)&y, sizeof(y));
+            return;
+        }
+
+        if (i > 0 && (i & 0xF0000000) != (i-1 & 0xF0000000)) {
+            float per = (float)(i - from) / (to - from) * 100;
+            printf("%5.2f%%\n", per);
+        }
+    }
+    printf("  100%%\n");
+    printf("CORRECT!\n");
 }
 
 int main()
 {
-    float_bits neg_inf = 0xFF800000;
-
-    float_half(neg_inf);
+    test_float_half(0x0FFFFFFF, 0xFFFFFFFF);
+    // size_t test = 0x01000001;
+    // test_float_half(test, test);
 
     return 0;
 }
